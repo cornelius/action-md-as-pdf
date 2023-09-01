dc2abedc976fbae675c96a680cea58b4d7cd96ca
Cornelius Schumacher <schumacher@kde.org>
2023-09-01 16:27:39 +0200

    Put out to folder auto-pdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ cat $INPUT_MARKDOWN_FILE
 echo "OUTPUT:"
 echo $INPUT_PDF_FILE
 
-pandoc --pdf-engine=wkhtmltopdf /github/workspace/${INPUT_MARKDOWN_FILE} -o /github/workspace/${INPUT_PDF_FILE}
+pandoc --pdf-engine=wkhtmltopdf /github/workspace/${INPUT_MARKDOWN_FILE} -o /github/workspace/auto-pdf/${INPUT_PDF_FILE}
