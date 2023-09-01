e65d254c6a2c365f143c169cdfade44f43633faf
Cornelius Schumacher <schumacher@kde.org>
2023-09-01 16:03:39 +0200

    Fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+echo $INPUT_MARKDOWN_FILE
 
-pandoc --pdf-engine=wkhtmltopdf -o /github/workspace/$(INPUT_MARKDOWN_FILE) /github/workspace/$(INPUT_PDF_FILE)
+pandoc --pdf-engine=wkhtmltopdf /github/workspace/${INPUT_MARKDOWN_FILE} -o /github/workspace/${INPUT_PDF_FILE}
