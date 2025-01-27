8b7308133e7ecb004091b51e5ccac95df0b03433
Cornelius Schumacher <schumacher@kde.org>
2025-01-27 12:50:46 +0100

    Set work dir so that images get resolved

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ if [ -z "$INPUT_MARKDOWN_FILE" ]
 then
       echo "DIR MODE"
       INPUTS=$(find /github/workspace/${INPUT_MARKDOWN_DIR} -maxdepth 1 -name "*.md" | sort | tr '\n' ' ')
+      cd /github/workspace/${INPUT_MARKDOWN_DIR}
 else
       echo "FILE MODE"
       INPUTS=/github/workspace/${INPUT_MARKDOWN_DIR}/${INPUT_MARKDOWN_FILE}
